f04fdc711acbdf5362ffe85f5f9019ef5c8da016
Stephen Fabian <fabianstephen@gmail.com>
2022-07-14 17:35:36 -0600

    completed main menu, setup, and computer class, working on player class

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,6 +4,9 @@ class Turn
   def initialize(comp_board, player_board)
     @comp_board = comp_board
     @player_board = player_board
-    @player_ship_sunk = 0
-    @cpu_ship_sunk = 0
+    # @player_ship_sunk = 0
+    # @cpu_ship_sunk = 0
   end
+
+end
+
--- runner.rb
+++ runner.rb
@@ -4,4 +4,23 @@ require './lib/board'
 require './lib/computer'
 require './lib/player'
 require './lib/game_manager'
+require './lib/turn'
 
+def start
+  game = GameManager.new
+  if game.welcome_message == "p"
+
+    game.setup
+
+    game_over = game.turn
+
+    while !game_over
+      game_over = game.turn
+    end
+  else
+    game.welcome_message
+  end
+
+end
+
+start
