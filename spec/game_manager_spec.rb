d27588d424c41b9b5ba394813054a783f3bff690
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-07-15 13:27:12 -0600

    refactor: final spec spacing fix

--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -7,25 +7,25 @@ require './game_manager'
 require './turn.rb'
 
 RSpec.describe GameManager do
-  game = GameManager.new
+    game = GameManager.new
 
 
-  it 'exists' do
-      expect(game).to be_an_instance_of(GameManager)
-      expect(game.comp_board).to be_an_instance_of(Board)
-  end
+    it 'exists' do
+        expect(game).to be_an_instance_of(GameManager)
+        expect(game.comp_board).to be_an_instance_of(Board)
+    end
 
-  xit 'initilizes comp and player board' do
-    expect(comp_board.render(true)).to eq("  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\n")
-    expect(player_board.render(true)).to eq("  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\n")
+    xit 'initilizes comp and player board' do
+      expect(comp_board.render(true)).to eq("  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\n")
+      expect(player_board.render(true)).to eq("  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\n")
 
-  end
+    end
 
 
-  xit 'players can place' do
-    player_board.place(play_cruiser,["A1", "B1", "C1"])
-    player_board.place(play_submarine, ["D1", "D2"])
-    expect(player_board.render(true)).to eq("  1 2 3 4\nA S . . .\nB S . . .\nC S . . .\nD S S . .\n")
-  end
+    xit 'players can place' do
+      player_board.place(play_cruiser,["A1", "B1", "C1"])
+      player_board.place(play_submarine, ["D1", "D2"])
+      expect(player_board.render(true)).to eq("  1 2 3 4\nA S . . .\nB S . . .\nC S . . .\nD S S . .\n")
+    end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,33 +1,33 @@
 require './lib/ship'
 
 RSpec.describe Ship do
-  cruiser = Ship.new("Cruiser", 3)
-
-  it 'exists and has a ship type and length' do
     cruiser = Ship.new("Cruiser", 3)
-    expect(cruiser).to be_an_instance_of(Ship)
-    expect(cruiser.name).to eq("Cruiser")
-    expect(cruiser.length).to eq(3)
-    expect(cruiser.health).to eq(cruiser.length)
-  end
 
-  it 'a ship is not sunk to begin with' do
-    expect(cruiser.sunk?).to eq(false)
-  end
+    it 'exists and has a ship type and length' do
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cruiser).to be_an_instance_of(Ship)
+      expect(cruiser.name).to eq("Cruiser")
+      expect(cruiser.length).to eq(3)
+      expect(cruiser.health).to eq(cruiser.length)
+    end
 
-  it 'can be hit, reducing health by 1' do 
-    cruiser.hit
+    it 'a ship is not sunk to begin with' do
+      expect(cruiser.sunk?).to eq(false)
+    end
 
-    expect(cruiser.health).to eq(2)
-    expect(cruiser.sunk?).to eq(false)
-  end
+    it 'can be hit, reducing health by 1' do 
+      cruiser.hit
 
-  it 'when ship health is 0, ship is sunk' do 
-    cruiser.hit
-    cruiser.hit
-    cruiser.hit
+      expect(cruiser.health).to eq(2)
+      expect(cruiser.sunk?).to eq(false)
+    end
 
-    expect(cruiser.sunk?).to eq(true)
-  end
+    it 'when ship health is 0, ship is sunk' do 
+      cruiser.hit
+      cruiser.hit
+      cruiser.hit
+
+      expect(cruiser.sunk?).to eq(true)
+    end
 
 end
