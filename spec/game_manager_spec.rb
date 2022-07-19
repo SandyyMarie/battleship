1f34a7a3af902ce5adc1f08e463a1c852226e41b
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-07-19 14:07:43 -0600

    test: added full coverage tests for game_manager_spec

--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -6,26 +6,26 @@ require './lib/player'
 require './game_manager'
 require './turn.rb'
 
-describe GameManager do
-game = GameManager.new
+RSpec.describe GameManager do
+  game = GameManager.new
 
-
-it 'exists' do
+  it 'exists' do
     expect(game).to be_an_instance_of(GameManager)
-    expect(game.comp_board).to be_an_instance_of(Board)
+    expect(game.computer.board).to be_an_instance_of(Board)
   end
 
-  xit 'initilizes comp and player board' do
-    expect(comp_board.render(true)).to eq("  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\n")
-    expect(player_board.render(true)).to eq("  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\n")
+  it 'initilizes comp and player board' do
+    expect(game.computer.board.render(true)).to eq("  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\n")
+    expect(game.player.board.render(true)).to eq("  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . .\n")
 
   end
 
-
-  xit 'players can place' do
-    player_board.place(play_cruiser,["A1", "B1", "C1"])
-    player_board.place(play_submarine, ["D1", "D2"])
-    expect(player_board.render(true)).to eq("  1 2 3 4\nA S . . .\nB S . . .\nC S . . .\nD S S . .\n")
+  it 'can trigger the end game when two ships have been sunk' do
+    game.player.increase_ship_sunk
+    game.player.increase_ship_sunk
+    expect(game.end_game).to eq(true)
+    #outputs "Aww the Computer wins!"
   end
+
 
 end
